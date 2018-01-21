cd35fdb8d100e3a072d687e2865d9d16fec4090d
ryoasu <ryoasu107@gmail.com>
2018-01-22 01:06:25 +0900

    Implement calculation of grad-cam

--- grad_cam.py
+++ grad_cam.py
@@ -1,38 +1,82 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+import importlib.machinery as im_machinery
+import os
+import types
+
+import cv2
 import keras_pkg.grad_cam as k_grad_cam
+from util.config import Config
 
 
-def cmd():
+def __cmd():
     parser = argparse.ArgumentParser()
-    parser.add_argument('image',
-                        type=str,
-                        help='set image path')
-    parser.add_argument('-w', '--weight',
-                        required=True,
-                        help='set weight file (h5) path')
-    parser.add_argument('-m', '--model',
-                        help='set architecture file (json or yml) path')
+    parser.add_argument('config_file', type=str, help='set config file (.yml)')
 
     return parser.parse_args()
 
 
-def main():
-    # command line parse
-    args = cmd()
+def __function_loader(mod_path, func_name, mod_name='mod'):
+    loader = im_machinery.SourceFileLoader(mod_name, mod_path)
+    mod = types.ModuleType(loader.name)
+    # import module
+    loader.exec_module(mod)
+    # Check whether module is defined in function
+    assert func_name in dir(mod), '{0} is undefined in {1}'.format(func_name, mod_path)
+    func_str = mod_name + '.' + func_name
 
-    # create target model for grad-cam
-    if args.model is None:
-        target = k_grad_cam.Model(args.weight)
+    return eval(func_str)
+
+
+def __get_filename(path):
+    return os.path.splitext(os.path.basename(path))
+
+
+def __keras_grad_cam(target):
+    # get model for grad-cam
+    if target.architecture is None:
+        model = k_grad_cam.get_model(target.params)
     else:
-        target = k_grad_cam.Model(args.weight, args.model)
-
+        model = k_grad_cam.get_model(target.params, target.architecture)
     # show model summary
-    target.model.summary()
+    model.summary()
 
     # grad-cam
-    target.grad_cam(args.image)
+    if target.preprocessing is None:
+        cam, heatmap = k_grad_cam.grad_cam(
+            model,
+            target.layer,
+            target.image['path'])
+    else:
+        preprocessing_func = __function_loader(
+            target.preprocessing['source'],
+            target.preprocessing['function'])
+
+        cam, heatmap = k_grad_cam.grad_cam(
+            model,
+            target.layer,
+            target.image['path'],
+            preprocessing_func)
+
+    # output file
+    img_name, extension = __get_filename(target.image['path'])
+    output_name = 'grad_cam-{0}-{1}{2}'.format(model.name, img_name, extension)
+    cv2.imwrite(output_name, cam)
+
+
+def main():
+    # command line parse
+    args = __cmd()
+    # get config
+    conf = Config(args.config_file)
+
+    # for keras
+    if conf.framework == 'keras':
+        __keras_grad_cam(conf.target)
+    # Unimplemented
+    else:
+        print('Unimplemented ' + conf.framework)
 
 
 if __name__ == '__main__':
--- keras_pkg/grad_cam.py
+++ keras_pkg/grad_cam.py
@@ -2,21 +2,29 @@
 
 import os
 
+from PIL import Image
 from keras.preprocessing import image
+from keras.layers.core import Lambda
+import keras.backend as K
 import keras.models as models
 import numpy as np
+import tensorflow as tf
+import cv2
+
+# set test phase
+K.set_learning_phase(0)
 
 
-class Model:
-    def __init__(self, weight_path, architecture_path=None):
-        if architecture_path is None:
-            self.model = models.load_model(weight_path)
-        else:
-            self.model = self.__load_architecture(architecture_path)
+def __load_image(path, size, preprocess=False):
+    img = image.load_img(path, target_size=size)
+    img_arr = image.img_to_array(img)
+    if preprocess:
+        img_arr = img_arr / 255.
+    return img_arr
 
-            self.model.load_weights(weight_path)
 
-    def __load_architecture(self, path):
+def get_model(weight_path, architecture_path=None):
+    def load_architecture(path):
         with open(path) as file:
             model_str = file.read()
         # for json file
@@ -26,13 +34,76 @@ class Model:
         else:
             return models.model_from_yaml(model_str)
 
-    def __load_image(self, path, scaling_value):
-        img_size = self.model.input_shape[1:-1]
-        img = image.load_img(path, target_size=img_size)
-        img_arr = image.img_to_array(img) / scaling_value
-        return np.expand_dims(img_arr, axis=0)
+    if architecture_path is None:
+        return models.load_model(weight_path)
+    else:
+        model = load_architecture(architecture_path)
+        model.load_weights(weight_path)
+        return model
 
-    def grad_cam(self, image_path, scaling_value=255.0):
-        target_image = self.__load_image(image_path, scaling_value)
-        prediction_result = self.model.predict(target_image)
-        predicted_class = np.argmax(prediction_result)
+
+def target_category_loss(x, category_index, nb_classes):
+    return tf.multiply(x, K.one_hot([category_index], nb_classes))
+
+
+def normalize(x):
+    # calculate RMS (root mean square)
+    rms = K.sqrt(K.mean(K.square(x)))
+    # normalize
+    return x / (rms + 1e-5)
+
+
+def relu(x):
+    return np.maximum(x, 0)
+
+
+def grad_cam(model, target_layer, image_path, preprocessing=None):
+    # target image for grad-cam
+    image_size = model.input_shape[1:-1]
+    if preprocessing is None:
+        preprocessed_image = __load_image(image_path, image_size, preprocess=True)
+    else:
+        preprocessed_image = preprocessing(image_path, image_size)
+    input_image = np.expand_dims(preprocessed_image, axis=0)
+
+    # get prediction result and
+    prediction_result = model.predict(input_image)
+    predicted_class = np.argmax(prediction_result)
+
+    # Add layers to model for grad-cam (target_model)
+    target_model = models.Sequential()
+    target_model.add(model)
+    nb_classes = target_model.output_shape[-1]
+    loss_layer = lambda x: target_category_loss(x, predicted_class, nb_classes)
+    target_model.add(Lambda(loss_layer, output_shape=lambda x: x))
+
+    # implement function for calculate gradient (gradient_function)
+    loss = K.sum(target_model.layers[-1].output)
+    conv_output = target_model.layers[0].get_layer(target_layer).output
+    grads = normalize(K.gradients(loss, conv_output)[0])
+    gradient_function = K.function([target_model.layers[0].input],[conv_output, grads])
+
+    # calculate gradient and output
+    output, grads_val = gradient_function([input_image])
+    output, grads_val = output[0, :], grads_val[0, :, :, :]
+
+    # calculate weights of feature map
+    weights = np.mean(grads_val, axis=(0, 1))
+
+    # calculate heatmap
+    cam = np.ones(output.shape[0:2], dtype=np.float32)
+    for i, w in enumerate(weights):
+        cam += w * output[:, :, i]
+    cam = cv2.resize(cam, (image_size[1], image_size[0]))
+    cam = relu(cam)
+    heatmap = cam / np.max(cam)
+
+    # load original image (pixcel value: [0..255])
+    origin_image = __load_image(image_path, image_size)
+
+    # create cam
+    cam = cv2.applyColorMap(np.uint8(255 * heatmap), cv2.COLORMAP_JET)
+    cam = np.float32(cam) + np.float32(origin_image)
+    cam = 255 * cam / np.max(cam)
+
+    return np.uint8(cam), heatmap
